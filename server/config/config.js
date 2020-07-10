abf3862ebc2bcbdf30af25f15d91f69c2d9b207d
karlaVane <kmoyonr@est.ups.edu.ec>
2020-07-10 13:08:56 -0500

    Subiendo a heroku v2

--- server/config/config.js
+++ server/config/config.js
@@ -6,5 +6,5 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
 //base de datos
 let urlDB;
-urlDB = 'mongodb+srv://KarlaAdmin:kvmr210897@cluster0.toul9.mongodb.net/Cluster0/prueba_grupo_tech'
+urlDB = 'mongodb+srv://KarlaAdmin:kvmr210897@cluster0.toul9.mongodb.net/Cluster0'
 process.env.URLDB = urlDB;
